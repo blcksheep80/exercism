d22ca5c959c989366dd33def2cb09efbcc715377
Jason Lamb <jason.lamb@globalreader.eu>
2024-11-23 12:26:46 +0200

    Finish freelancer-rates

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,40 @@
 defmodule FreelancerRates do
+  @spec daily_rate(number()) :: float()
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    Kernel.*(hourly_rate, 8.0)
   end
 
-  def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+  @spec apply_discount(number(), number()) :: float()
+  def apply_discount(original_price, discount) do
+    discount = Kernel./(discount, 100)
+    percentage_remaining = Kernel.-(1, discount)
+
+    Kernel.*(original_price, percentage_remaining)
   end
 
+  @spec monthly_rate(number(), number()) :: integer()
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    discount = Kernel.*(discount, 1.0)
+
+    hourly_rate
+    |> Kernel.*(1.0)
+    |> daily_rate()
+    |> apply_discount(discount)
+    |> Kernel.*(22)
+    |> Kernel.ceil()
   end
 
+  @spec days_in_budget(number(), number(), number()) :: float()
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    budget = Kernel.*(budget, 1.0)
+    discount = Kernel.*(discount, 1.0)
+
+    daily_rate =
+      hourly_rate
+      |> daily_rate()
+      |> apply_discount(discount)
+
+    Kernel./(budget, daily_rate)
+    |> Float.floor(1)
   end
 end
