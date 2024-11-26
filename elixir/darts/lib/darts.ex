85d285a3093f4a903dee4dd364ad56d289d697b3
Jason Lamb <jason.lamb@globalreader.eu>
2024-11-26 22:05:28 +0200

    Finish darts

--- elixir/darts/lib/darts.ex
+++ elixir/darts/lib/darts.ex
@@ -6,5 +6,13 @@ defmodule Darts do
   """
   @spec score(position) :: integer
   def score({x, y}) do
+    distance = :math.sqrt(x * x + y * y)
+
+    cond do
+      distance > 10 -> 0
+      distance > 5 -> 1
+      distance > 1 -> 5
+      true -> 10
+    end
   end
 end
