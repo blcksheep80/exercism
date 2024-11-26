33a6f15317326e00c3e6542d12689802f0d1381d
Jason Lamb <jason.lamb@globalreader.eu>
2024-11-26 21:27:29 +0200

    Finish log-level

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,9 +1,31 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    cond do
+      level == 0 and not legacy? == true -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 and not legacy? == true -> :fatal
+      true -> :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    label = to_label(level, legacy?)
+
+    cond do
+      label in [:error, :fatal] ->
+        :ops
+
+      label == :unknown and legacy? == true ->
+        :dev1
+
+      label == :unknown ->
+        :dev2
+
+      true ->
+        false
+    end
   end
 end
